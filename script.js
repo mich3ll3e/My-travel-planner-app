299f7855970bda2e729156545d7d242a46f5ec15
R-A-exe <royatalla.ra@gmail.com>
2021-01-05 17:10:02 -0500

    mvp layout added, weather api added, hotel api added

--- script.js
+++ script.js
@@ -28,6 +28,10 @@ $.post("https://test.api.amadeus.com/v1/security/oauth2/token", { "grant_type": 
         }
     });
 
+    $.get("https://test.api.amadeus.com/v2/shopping/hotel-offers?cityCode=LCY&checkInDate=2021-05-01&checkOutDate=2021-05-28&adults=1").then((result)=>{
+        console.log(result);
+    })
+
     $.get("https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=YOW&destinationLocationCode=GRU&departureDate=2021-05-01&returnDate=2021-05-28&adults=1&travelClass=ECONOMY&nonStop=false&currencyCode=CAD&max=10").then((results) => {
         console.log(results);
     })
@@ -35,4 +39,15 @@ $.post("https://test.api.amadeus.com/v1/security/oauth2/token", { "grant_type": 
     $.get("https://test.api.amadeus.com/v1/reference-data/locations?subType=AIRPORT&keyword=Toronto&page%5Blimit%5D=10&sort=analytics.travelers.score&view=LIGHT").then((results) => {
         console.log(results);
     })
+
+})
+
+//For historical data
+$.get("https://api.worldweatheronline.com/premium/v1/past-weather.ashx?q=London&date=2020-01-01&enddate=2020-01-07&tp=24&format=json&key=6dda14a8cc53490d9fd201404210301").then((result)=>{
+    console.log(result);
+})
+
+//For next 15 days
+$.get("http://api.worldweatheronline.com/premium/v1/weather.ashx?q=Ottawa&date=2021-01-07&tp=24&format=json&key=6dda14a8cc53490d9fd201404210301").then((result)=>{
+    console.log(result);
 })
